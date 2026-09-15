eeb2942116daedc485b961c53c4db895106dde8d
itisacrouton <itisacrouton@proton.me>
2026-09-15 16:06:59 +1000

    asbtracted more of the underlying lcd_lib

--- display.c
+++ display.c
@@ -4,24 +4,41 @@ void display_init(){
     lcd_init(&my_lcd, 0x27, 4, 5, i2c0);
 }
 
-void display_top_line(char* string){
-    lcd_set_cursor_pos(&my_lcd, 0, 0);
+void display_set_pos(uint8_t line, uint8_t index){
+    lcd_set_cursor_pos(&my_lcd, line, index);
+}
+
+void display_write(char* string){
     lcd_string(&my_lcd, string);
+}
+
+void display_char(char c){
+    lcd_char(&my_lcd, c);
+}
+
+void display_text(char* string, uint8_t line, uint8_t index){
+    display_set_pos(line, index);
+    display_write(string);
+}
+
+void display_top_line(char* string){
+    display_set_pos(0, 0);
+    display_write(string);
 }
 
 void display_bottom_line(char* string){
-    lcd_set_cursor_pos(&my_lcd, 1, 0);
-    lcd_string(&my_lcd, string);
+    display_set_pos(1, 0);
+    display_write(string);
 }
 
 void display_clear_top(){
-    lcd_set_cursor_pos(&my_lcd, 0, 0);
-    lcd_string(&my_lcd, "                ");
+    display_set_pos(0, 0);
+    display_write("                ");
 }
 
 void display_clear_bottom(){
-    lcd_set_cursor_pos(&my_lcd, 1, 0);
-    lcd_string(&my_lcd, "                ");
+    display_set_pos(1, 0);
+    display_write("                ");
 }
 
 void display_clear(){
