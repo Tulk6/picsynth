d28fb768ddd174a892634df5cc291b4c7fa24451
itisacrouton <itisacrouton@proton.me>
2026-09-15 16:07:26 +1000

    gave filters both oscillator and fixed intensities

--- filter.c
+++ filter.c
@@ -10,12 +10,14 @@ struct Filter {
     enum FilterType type;
     int16_t prev_sample;
     struct Oscillator* intensity_oscillator;
+    int16_t intensity_value;
 };
 
 void filter_init(struct Filter* filter){
     filter->prev_sample = 0;
     filter->type = TEST;
     filter->intensity_oscillator = NULL;
+    filter->intensity_value = 0;
 }
 
 struct Filter* filter_new(){
@@ -24,13 +26,16 @@ struct Filter* filter_new(){
     return filter;
 }
 
+int16_t filter_get_intensity(struct Filter* filter){
+    if (filter->intensity_oscillator != NULL){
+        return oscillator_get_current_sample(filter->intensity_oscillator);
+    }
+    return filter->intensity_value;
+}
+
 int16_t filter_apply(struct Filter* filter, int16_t sample){
     int64_t new_sample = 0;
-    int16_t intensity = 0;
-    if (filter->intensity_oscillator != NULL){
-        
-    } intensity = oscillator_get_current_sample(filter->intensity_oscillator);
-    //if (intensity != 0) printf("Filter: %i\n", intensity);
+    int16_t intensity = filter_get_intensity(filter);
     switch (filter->type){
         case TEST:
             new_sample = ((intensity*sample)>>15) + (((32767-intensity)*filter->prev_sample)>>15);
